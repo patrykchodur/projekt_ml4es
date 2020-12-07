781c25fc6f76e30137d00be5be8e0db8bc9d8003
Patryk Chodur <patrykchodur@icloud.com>
2020-12-07 23:05:43 +0100

    init commit

---
+++ app/src/main/java/com/example/projekt_ml4es/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projekt_ml4es;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
